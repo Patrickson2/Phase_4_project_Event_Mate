5108090699672711e96f84205db4413d4ecdf43b
Kenneth-Kipkosgei <kennethkipkosgei38@gmail.com>
2026-01-15 17:39:23 +0300

    Resolve local change in App.jsx before merging to main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,4 @@
-HEAD
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-e125d4fb70b6f1e6ab02a4df294161ed7be7db53
 import { AuthProvider } from './context/AuthContext';
 import Navbar from './components/common/Navbar';
 import Footer from './components/common/Footer';
@@ -19,7 +16,6 @@ import './styles/App.css';
 
 function App() {
   return (
- HEAD
     <Router>
       <AuthProvider>
         <div className="app">
@@ -41,40 +37,6 @@ function App() {
         </div>
       </AuthProvider>
     </Router>
-    <AuthProvider>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/events/:id" element={
-            <ProtectedRoute>
-              <EventDetail />
-            </ProtectedRoute>
-          } />
-          <Route path="/dashboard" element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          } />
-          <Route path="/my-events" element={
-            <ProtectedRoute>
-              <MyEvents />
-            </ProtectedRoute>
-          } />
-          <Route path="/profile" element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          } />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </AuthProvider>
- e125d4fb70b6f1e6ab02a4df294161ed7be7db53
   );
 }
 
