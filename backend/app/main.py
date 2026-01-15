2f5091dbb1756b918366f1b518afadcc56147338
Bonarerijt <prudyjj4@gmail.com>
2026-01-15 14:36:43 +0300

    added necessary imports to main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,8 @@
-# TODO: Implement
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .database import engine, Base
+from .routers import auth_router, events_router, participation_router, reviews_router
+
+Base.metadata.create_all(bind=engine)
+
+
